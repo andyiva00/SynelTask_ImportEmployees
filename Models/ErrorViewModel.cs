b0fa692bef84647d45a047c54526079b139b855c
Andrey Ivanov <andyiva00@gmail.com>
2023-09-14 13:04:12 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SynelTask_ImportEmployees.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
